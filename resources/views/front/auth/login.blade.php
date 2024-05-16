f4f9023a9f322635ebd2b7bc3db65175436719ab
Diaamohammed74 <Diaa.Mohamed2021388@commerce.helwan.edu.eg>
2024-05-16 13:40:06 +0300

    login with google

--- resources/views/front/auth/login.blade.php
+++ resources/views/front/auth/login.blade.php
@@ -60,8 +60,38 @@
                                 <!-- br for or -->
                                 <div class="line"></div>
                                 <!-- SocialGroupButton -->
-                                @include('front.partials.auth.social')
+                                {{-- @include('front.partials.auth.social') --}}
 
+
+                                <div class="row justify-content-center">
+                                    <div class="col-md-4">
+                                        <div class="mb-4 d-flex justify-content-center">
+                                            <a href="{{ route('auth.socialite.login', ['provider' => 'facebook']) }}"data-mdb-button-init data-mdb-ripple-init
+                                                class="btn btn-lg face-btn">
+                                                <i class="fa-brands fa-facebook-f" style="color: #1968f0;"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="mb-4 d-flex justify-content-center">
+                                            <a href="{{ route('auth.socialite.login', ['provider' => 'google']) }}"
+                                                data-mdb-button-init data-mdb-ripple-init
+                                                class="btn btn-light btn-lg google-btn">
+                                                <img src="{{ asset('assets-front/images/Google.png') }}">
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="mb-4 d-flex justify-content-center">
+                                            <button type="button" data-mdb-button-init data-mdb-ripple-init
+                                                class="btn btn-dark btn-lg ios-btn">
+                                                <i class="fa-brands fa-apple" style="color: #ffffff;"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                
                             </div>
                         </div>
                     </div>
--- resources/views/front/auth/register.blade.php
+++ resources/views/front/auth/register.blade.php
@@ -144,14 +144,6 @@
                                             </a>
                                         </div>
                                     </div>
-
-
-
-                                    {{-- <div class="text-center" style="margin: 20px;">
-            <a href="{{ route('socialite.login', ['provider' => 'github']) }}"
-                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Login
-                With GitHub</a>
-        </div> --}}
                                     <div class="col-md-4">
                                         <div class="mb-4 d-flex justify-content-center">
                                             <button type="button" data-mdb-button-init data-mdb-ripple-init
