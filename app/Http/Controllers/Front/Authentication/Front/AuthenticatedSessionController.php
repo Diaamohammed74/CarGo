877d68378666ae918fe30a112d6e3ad8ae37304d
Diaamohammed74 <Diaa.Mohamed2021388@commerce.helwan.edu.eg>
2024-05-19 02:08:08 +0300

    update loggedout message

--- app/Http/Controllers/Front/Authentication/Front/AuthenticatedSessionController.php
+++ app/Http/Controllers/Front/Authentication/Front/AuthenticatedSessionController.php
@@ -37,11 +37,9 @@ class AuthenticatedSessionController extends Controller
     public function destroy(Request $request): RedirectResponse
     {
         Auth::guard('web')->logout();
-
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
-        toast('Loggedout successfuly','warning');
+        toast('We hope to see you back on the road soon.keep your ride smooth!','warning');
         return redirect()->intended(RouteServiceProvider::HOME);
     }
 }
